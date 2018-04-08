e1e4d34e226e00edbb9a796ec074ee61a3b19079
Albert Agram <albert.agram@gmail.com>
2018-04-08 20:41:22 +0000

    fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react'
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
 class BooksApp extends Component {
   state = {
     /**
