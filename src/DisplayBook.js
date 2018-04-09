d616eee79b1aecb56d1befc0ee3e5c2c6b2b222f
Albert Agram <albert.agram@gmail.com>
2018-04-09 12:16:28 +0000

    handle books with no authors edge case

--- src/DisplayBook.js
+++ src/DisplayBook.js
@@ -18,7 +18,7 @@ function DisplayBook ({ book, onChangeShelf })  {
         </div>
       </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors.join(', ')}</div>
+      <div className="book-authors">{book.authors ? book.authors.join(', ') : ""}</div>
     </div>
   )
 }
