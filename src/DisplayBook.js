e5df9c2ebba2e5116fd299a2e39324b59a502432
Albert Agram <albert.agram@gmail.com>
2018-04-09 12:15:54 +0000

    handle books with no thumbnail bug

--- src/DisplayBook.js
+++ src/DisplayBook.js
@@ -6,7 +6,7 @@ function DisplayBook ({ book, onChangeShelf })  {
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: (book.imageLinks ? `url(${book.imageLinks.thumbnail})` : "") }}></div>
         <div className="book-shelf-changer">
           <select value={book.shelf} onChange={ (e) => onChangeShelf(book, e.target.value) }>
             <option value="none" disabled>Move to...</option>
