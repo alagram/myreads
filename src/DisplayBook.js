96349fe80dda9eb70a0c07e4724a71c19528e8c7
Albert Agram <albert.agram@gmail.com>
2018-04-10 15:11:21 +0000

    clean up DisplayBook component
    - destruture new props
    - generalize template
    - filter and map over books prop to display books

--- src/DisplayBook.js
+++ src/DisplayBook.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function DisplayBook ({ book, onChangeShelf })  {
+function DisplayBook ({ shelf, title, books, onChangeShelf })  {
 
   return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: (book.imageLinks ? `url(${book.imageLinks.thumbnail})` : "") }}></div>
-        <div className="book-shelf-changer">
-          <select value={book.shelf ? book.shelf : 'none'} onChange={ (e) => onChangeShelf(book, e.target.value) }>
-            <option value="moveTo" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+    <div className="list-books-content">
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books.filter( (book) => book.shelf === shelf).map((book) => (
+                  <li key={book.id}>
+                    <div  className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: (book.imageLinks ? `url(${book.imageLinks.thumbnail})` : "") }}></div>
+                        <div className="book-shelf-changer">
+                          <select value={book.shelf ? book.shelf : 'none'} onChange={ (e) => onChangeShelf(book, e.target.value) }>
+                            <option value="moveTo" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors ? book.authors.join(', ') : ""}</div>
+                    </div>
+                  </li>
+
+
+                ))}
+              </ol>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors ? book.authors.join(', ') : ""}</div>
     </div>
   )
 }
 
 DisplayBook.propTypes = {
-  book: PropTypes.object.isRequired,
+  shelf: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
   onChangeShelf: PropTypes.func.isRequired
 }
 
